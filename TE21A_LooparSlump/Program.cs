aa039d906d24a85173e07a001930ad112497a9c2
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-27 13:42:01 +0200

    Initial commit

---
+++ TE21A_LooparSlump/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random generator = new Random();
+
+while (true)
+{
+  int result = generator.Next(5, 15);
+  Console.WriteLine(result);
+
+  // if (result == 0)
+  // {
+  //   Console.WriteLine("Du får en banan");
+  // }
+  // if (result == 1)
+  // {
+  //   Console.WriteLine("Du får en annan banan");
+  // }
+  Console.ReadLine();
+}
+
+// int hp = 5;
+
+// while (hp > 0)
+// {
+//   Console.WriteLine("Still alive!");
+//   Console.WriteLine(hp);
+//   // hp--;
+// }
+Console.ReadLine();
